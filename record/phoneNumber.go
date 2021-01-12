6c9052ebb870de89a644e3fde1cc03d359d4d3a7
8bitstout <conor.murphy4304@gmail.com>
2021-01-12 05:49:25 -0500

    make phone number error more descriptive

--- record/phoneNumber.go
+++ record/phoneNumber.go
@@ -14,7 +14,7 @@ func NewPhoneNumber(pn string) (PhoneNumber, error) {
 	match, _ := regexp.MatchString("^[0-9]\\d{2}-\\d{3}-\\d{4}$", pn)
 	if !match {
 		fmt.Println("Invalid phone number:", pn)
-		return "", errors.New("invalid phone number provided")
+		return "", errors.New("invalid phone number provided. Phone number expected to be in the following format: ###-###-####")
 	}
 	return PhoneNumber(pn), nil
 }
