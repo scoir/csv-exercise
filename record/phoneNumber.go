a47e311e9b3c7e1250272be188a29481157917e9
8bitstout <conor.murphy4304@gmail.com>
2021-01-12 08:38:39 -0500

    remove log

--- record/phoneNumber.go
+++ record/phoneNumber.go
@@ -2,7 +2,6 @@ package record
 
 import (
 	"errors"
-	"fmt"
 	"regexp"
 )
 
@@ -13,7 +12,6 @@ type PhoneNumber string
 func NewPhoneNumber(pn string) (PhoneNumber, error) {
 	match, _ := regexp.MatchString("^[0-9]\\d{2}-\\d{3}-\\d{4}$", pn)
 	if !match {
-		fmt.Println("Invalid phone number:", pn)
 		return "", errors.New("invalid phone number provided. Phone number expected to be in the following format: ###-###-####")
 	}
 	return PhoneNumber(pn), nil
